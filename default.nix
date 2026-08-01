475f07f5e027d5a9a96ed6fe3bb215b87827ff52
Socheat <github@socheat.dev>
2026-08-01 18:01:27 +0700

    Update example packages

--- default.nix
+++ default.nix
@@ -2,9 +2,58 @@
   pkgs ? import <nixpkgs> { },
 }:
 rec {
+  hello-image = pkgs.dockerTools.buildImage {
+    name = "hello-image";
+    tag = "dev";
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      pathsToLink = [ "/bin" ];
+      paths = [ pkgs.hello ];
+    };
+    config = {
+      Cmd = [ "${pkgs.hello}/bin/hello" ];
+    };
+  };
+
   hello-layered-image = pkgs.dockerTools.buildLayeredImage {
     name = "hello-layered-image";
     tag = "dev";
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      pathsToLink = [ "/bin" ];
+      paths = [ pkgs.hello ];
+    };
+    config = {
+      Cmd = [ "${pkgs.hello}/bin/hello" ];
+    };
+  };
+
+  hello-image-with-nix-db = pkgs.dockerTools.buildImageWithNixDb {
+    name = "hello-image-with-nix-db";
+    tag = "dev";
+  
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      pathsToLink = [ "/bin" ];
+      paths = [
+        # nix-store uses cat program to display results as specified by
+        # the image env variable NIX_PAGER.
+        pkgs.coreutils
+        pkgs.nix
+        pkgs.bash
+        pkgs.hello
+      ];
+    };
+
+    config = {
+      Env = [
+        "NIX_PAGER=cat"
+        # A user is required by nix
+        # https://github.com/NixOS/nix/blob/9348f9291e5d9e4ba3c4347ea1b235640f54fd79/src/libutil/util.cc#L478
+        "USER=nobody"
+      ];
+    };
+
     config = {
       Cmd = [ "${pkgs.hello}/bin/hello" ];
     };
--- docker-bake.hcl
+++ docker-bake.hcl
@@ -1,10 +1,33 @@
 target "default" {
     context = "."
     dockerfile = "flake.nix"
+    tags = [ "socheatsok78/buildkit-nix-demo:default" ]
+}
+
+target "hello-image" {
+    context = "."
+    target = "hello-image"
+    dockerfile = "flake.nix"
+    tags = [ "socheatsok78/buildkit-nix-demo:hello-image" ]
 }
 
 target "hello-layered-image" {
     context = "."
     target = "hello-layered-image"
     dockerfile = "flake.nix"
+    tags = [ "socheatsok78/buildkit-nix-demo:hello-layered-image" ]
+}
+
+target "hello-image-with-nix-db" {
+    context = "."
+    target = "hello-image-with-nix-db"
+    dockerfile = "flake.nix"
+    tags = [ "socheatsok78/buildkit-nix-demo:hello-image-with-nix-db" ]
+}
+
+target "default-multi-platform" {
+    context = "."
+    dockerfile = "flake.nix"
+    platforms = ["linux/amd64", "linux/arm64"]
+    tags = [ "socheatsok78/buildkit-nix-demo:multi-platform" ]
 }
